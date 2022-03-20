456b40f9f97112fae04821f58a23f45619247be6
muhammadalsattar <muhammadalsattar@gmail.com>
2022-03-20 12:48:06 +0200

    fix token security issue

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 import request from 'request'
 
 const forecast = (coords, callback)=>{
-    request(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=6decea5f402964f2a9dcef3edccbdfa1&units=metric`, {json: true}, (err, res)=>{
+    request(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=${process.env.OWMAPPID}&units=metric`, {json: true}, (err, res)=>{
         if(err)
         {
             return callback(undefined, err)
--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -1,7 +1,7 @@
 import request from "request"
 
 const geolocation = (place, callback)=>{
-    request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?limit=1&access_token=pk.eyJ1IjoibXVoYW1tYWRhbHNhdHRhciIsImEiOiJja3preWJ2am0wbWthMm5xcjlqazRhMTU5In0.MSdN_n6QklZA2k39eH1rFA`, {json: true}, (err, res)=>{
+    request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?limit=1&access_token=${process.env.MAPBOXTOKEN}`, {json: true}, (err, res)=>{
         if (err)
         {
             return callback(undefined, err)
