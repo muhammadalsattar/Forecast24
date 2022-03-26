decf3746130f5973e3ae2ac147323fc273636351
muhammadalsattar <muhammadalsattar@gmail.com>
2022-03-26 17:30:03 +0200

    update ui

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,9 @@ const form = document.querySelector('form')
 const temperature = document.querySelector('.temperature-location h1')
 const mylocation = document.querySelector('.temperature-location p')
 const input = document.querySelector('form input')
+const description = document.querySelector('.description')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
 
 
 
@@ -14,6 +17,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
         .then(data=>{
             temperature.textContent = `${Math.round(data.data.main.temp)}º`
             mylocation.textContent = `${data.data.name}, ${data.data.sys.country}`
+            description.textContent = `${data.data.weather[0].description}`
+            humidity.textContent = `Humidity: ${data.data.main.humidity}%`
+            wind.textContent = `Wind: ${data.data.wind.speed} m/s`
         })
     })
 })
@@ -23,8 +29,12 @@ form.addEventListener('submit', (e)=>{
     fetch(`/weather?address=${input.value}`)
     .then(response => response.json())
     .then(data =>{
+        console.log(data.data)
         temperature.textContent = `${Math.round(data.data.main.temp)}º`
         mylocation.textContent = `${data.data.name}, ${data.data.sys.country}`
+        description.textContent = `${data.data.weather[0].description}`
+        humidity.textContent = `Humidity: ${data.data.main.humidity}%`
+        wind.textContent = `Wind: ${data.data.wind.speed} m/s`
     })
     input.value = ''
 })
--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -1,4 +1,5 @@
 import request from "request"
+import 'dotenv/config'
 
 const geolocation = (place, callback)=>{
     request(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?limit=1&access_token=${process.env.MAPBOXTOKEN}`, {json: true}, (err, res)=>{
